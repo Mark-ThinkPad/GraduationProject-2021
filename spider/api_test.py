881fa119d325e0b0211dcc12d3fd9fa9e8d489ea
Mark-ThinkPad <1372469698@qq.com>
2021-01-14 19:21:10 +0800

    2021-01-14 19:21:10

--- spider/api_test.py
+++ spider/api_test.py
@@ -1,14 +1,12 @@
 import json
 import requests
 
-url = 'https://club.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98' \
-      '&productId=100009958343&score=0&sortType=5&page=0&pageSize=10&isShadowSku=0&fold=1'
+url = 'https://club.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98&' \
+      'productId=5561746&score=0&sortType=5&page={}&pageSize=10&isShadowSku=0&fold=1'
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                  'Chrome/87.0.4280.141 Safari/537.36',
-    'Accept': '*/*',
-    'Referer': 'https://item.jd.com/100009958343.html'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'
 }
 
 res = requests.get(url, headers)
