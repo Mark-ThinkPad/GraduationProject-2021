86f3213ce1662ca5da6be361969b83ccf38fb2fa
Mark-ThinkPad <1372469698@qq.com>
2021-03-23 21:02:03 +0800

    2021-03-23 21:02:03

--- data_analyze/mi10_analyze.py
+++ data_analyze/mi10_analyze.py
@@ -1,10 +1,10 @@
 from db.mi10_models import Comment, CommentSummary, ModelSummary
-from db.mi10_analyze_models import UserDeviceSummary
+from db.mi10_analyze_models import UserDeviceCount, Total
 from data_analyze.utils import calculate_percentage
 
 
 # 分析安卓用户留存率和iOS用户转化率
-def get_user_device_summary():
+def get_user_device_count():
     total = Comment.select().where(Comment.user_device.is_null(False)).count()
     android = Comment.select().where(Comment.user_device == 'Android').count()
     ios = Comment.select().where(Comment.user_device == 'iOS').count()
@@ -13,7 +13,7 @@ def get_user_device_summary():
     ios_percentage = calculate_percentage(total, ios)
     other_percentage = calculate_percentage(total, other)
 
-    UserDeviceSummary.create(
+    UserDeviceCount.create(
         total=total,
         android=android,
         ios=ios,
@@ -24,13 +24,118 @@ def get_user_device_summary():
     )
 
 
-# 简单处理一下统计总数
+# 简单处理一下评论统计总数
 def get_total():
-    total = 0
+    get_jd_total()
+    get_sn_total()
+    get_mishop_total()
+
+
+# 计算京东平台的评论统计总数
+def get_jd_total():
+    jd_comments_summary = CommentSummary.get(CommentSummary.source == '京东')
+    jd_total = jd_comments_summary.default_good + jd_comments_summary.star_one + jd_comments_summary.star_two + \
+               jd_comments_summary.star_three + jd_comments_summary.star_four + jd_comments_summary.star_five
+    jd_good_count = jd_comments_summary.star_four + jd_comments_summary.star_five
+    jd_general_cont = jd_comments_summary.star_two + jd_comments_summary.star_three
+    jd_bad_count = jd_comments_summary.star_one
+    jd_cal_total = jd_comments_summary.star_one + jd_comments_summary.star_two + jd_comments_summary.star_three + \
+                   jd_comments_summary.star_four + jd_comments_summary.star_five
+    jd_good_rate = calculate_percentage(jd_cal_total, jd_good_count)
+    jd_models_total = 0
     for ms in ModelSummary.select().where(ModelSummary.source == '京东'):
-        total += ms.default_good + ms.star_one + ms.star_two + ms.star_three + ms.star_four + ms.star_five
-    print(total)
+        jd_models_total += ms.default_good + ms.star_one + ms.star_two + ms.star_three + ms.star_four + ms.star_five
+
+    Total.create(
+        source='京东',
+        total=jd_total,
+        all_models_total=jd_models_total,
+        good_rate=jd_good_rate,
+        default_good=jd_comments_summary.default_good,
+        good_count=jd_good_count,
+        general_count=jd_general_cont,
+        bad_count=jd_bad_count,
+        star_one=jd_comments_summary.star_one,
+        star_two=jd_comments_summary.star_two,
+        star_three=jd_comments_summary.star_three,
+        star_four=jd_comments_summary.star_four,
+        star_five=jd_comments_summary.star_five
+    )
+
+
+# 计算苏宁平台的评论统计总数
+def get_sn_total():
+    sn_comments_summary = CommentSummary.get(CommentSummary.source == '苏宁')
+    sn_good_count = sn_comments_summary.star_four + sn_comments_summary.star_five
+    sn_general_cont = sn_comments_summary.star_two + sn_comments_summary.star_three
+    sn_bad_count = sn_comments_summary.star_one
+    sn_models_total = 0
+    for ms in ModelSummary.select().where(ModelSummary.source == '苏宁'):
+        sn_models_total += ms.total
+
+    Total.create(
+        source='苏宁',
+        total=sn_comments_summary.total,
+        all_models_total=sn_models_total,
+        good_rate=sn_comments_summary.good_rate,
+        default_good=sn_comments_summary.default_good,
+        good_count=sn_good_count,
+        general_count=sn_general_cont,
+        bad_count=sn_bad_count,
+        star_one=sn_comments_summary.star_one,
+        star_two=sn_comments_summary.star_two,
+        star_three=sn_comments_summary.star_three,
+        star_four=sn_comments_summary.star_four,
+        star_five=sn_comments_summary.star_five
+    )
+
+
+# 计算小米商城的评论统计总数
+def get_mishop_total():
+    mishop_comments_summary = CommentSummary.get(CommentSummary.source == '小米商城')
+    mishop_good_count = mishop_comments_summary.star_three + mishop_comments_summary.star_four + \
+                        mishop_comments_summary.star_five
+    mishop_general_cont = mishop_comments_summary.star_two
+    mishop_bad_count = mishop_comments_summary.star_one
+
+    Total.create(
+        source='小米商城',
+        total=mishop_comments_summary.total,
+        all_models_total=mishop_comments_summary.total,
+        good_rate=mishop_comments_summary.good_rate,
+        default_good=mishop_comments_summary.default_good,
+        good_count=mishop_good_count,
+        general_count=mishop_general_cont,
+        bad_count=mishop_bad_count,
+        star_one=mishop_comments_summary.star_one,
+        star_two=mishop_comments_summary.star_two,
+        star_three=mishop_comments_summary.star_three,
+        star_four=mishop_comments_summary.star_four,
+        star_five=mishop_comments_summary.star_five
+    )
+
+
+# 计算各个型号的统计数据
+def get_model_count():
+    all_color = ['国风雅灰', '钛银黑', '冰海蓝', '蜜桃金']
+    all_ram = ['8GB', '12GB']
+    all_rom = ['128GB', '256GB']
+    for color in all_color:
+        for ram in all_ram:
+            for rom in all_rom:
+                all_ms = ModelSummary.select().where(
+                    (ModelSummary.product_color == color) &
+                    (ModelSummary.product_ram == ram) &
+                    (ModelSummary.product_rom == rom)
+                )
+                for ms in all_ms:
+                    if ms.source == '京东':
+                        pass
+                    if ms.source == '苏宁':
+                        pass
 
 
 if __name__ == '__main__':
-    get_total()
+    # get_user_device_count()
+    # get_total()
+    get_model_count()
--- db/mi10_analyze_models.py
+++ db/mi10_analyze_models.py
@@ -9,8 +9,8 @@ class BaseModel(pw.Model):
         database = mi10_analyze_db
 
 
-# 用户发表评论时使用的设备类型, 用于计算安卓用户留存率和iOS用户转化率
-class UserDeviceSummary(BaseModel):
+# 统计用户发表评论时使用的设备类型, 用于计算安卓用户留存率和iOS用户转化率
+class UserDeviceCount(BaseModel):
     total = pw.IntegerField()
     android = pw.IntegerField()
     ios = pw.IntegerField()
@@ -32,12 +32,37 @@ class Total(BaseModel):
     good_count = pw.IntegerField()  # 好评数
     general_count = pw.IntegerField()  # 中评数
     bad_count = pw.IntegerField()  # 差评数
+    star_one = pw.IntegerField()  # 一星数量
+    star_two = pw.IntegerField()  # 二星数量
+    star_three = pw.IntegerField()  # 三星数量
+    star_four = pw.IntegerField()  # 四星数量
+    star_five = pw.IntegerField()  # 五星数量
+
+
+# 各个型号的统计数据 (苏宁和京东数据合并)
+class ModelCount(BaseModel):
+    # 产品型号信息
+    product_color = pw.CharField(max_length=4,
+                                 constraints=[pw.Check('product_color in ("国风雅灰", "钛银黑", "冰海蓝", "蜜桃金")')])  # 产品颜色版本
+    product_ram = pw.CharField(max_length=4, constraints=[pw.Check('product_ram in ("8GB", "12GB")')])  # 内存大小
+    product_rom = pw.CharField(max_length=5, constraints=[pw.Check('product_rom in ("128GB", "256GB")')])  # 储存大小
+    total = pw.IntegerField(null=True)
+    percentage = pw.CharField(max_length=4, null=True)  # 该型号占总数的百分比
+    cal_total = pw.IntegerField(null=True)  # 用于计算好评率的总数 (京东计算规则)
+    good_rate = pw.CharField(max_length=4, null=True)  # 好评率
+    default_good = pw.IntegerField(null=True)  # 默认好评数
+    good_count = pw.IntegerField(null=True)  # 好评数
+    general_count = pw.IntegerField(null=True)  # 中评数
+    bad_count = pw.IntegerField(null=True)  # 差评数
     star_one = pw.IntegerField(null=True)  # 一星数量
     star_two = pw.IntegerField(null=True)  # 二星数量
     star_three = pw.IntegerField(null=True)  # 三星数量
     star_four = pw.IntegerField(null=True)  # 四星数量
     star_five = pw.IntegerField(null=True)  # 五星数量
 
+    class Meta:
+        primary_key = pw.CompositeKey('product_color', 'product_ram', 'product_rom')  # 复合主键
+
 
 if __name__ == '__main__':
-    mi10_analyze_db.create_tables([Total])
+    mi10_analyze_db.create_tables([UserDeviceCount, Total, ModelCount])
