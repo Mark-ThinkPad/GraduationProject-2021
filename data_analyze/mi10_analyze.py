61fb4afb76016d1a976317947f08896bf27adca7
Mark-ThinkPad <1372469698@qq.com>
2021-03-24 17:06:43 +0800

    2021-03-24 17:06:43

--- data_analyze/mi10_analyze.py
+++ data_analyze/mi10_analyze.py
@@ -1,9 +1,10 @@
+from time import strftime
 from db.mi10_models import Comment, CommentSummary, ModelSummary
-from db.mi10_analyze_models import UserDeviceCount, Total
+from db.mi10_analyze_models import (UserDeviceCount, Total, ModelCount, ColorCount, RamCount, RomCount)
 from data_analyze.utils import calculate_percentage
 
 
-# 分析安卓用户留存率和iOS用户转化率
+# 计算安卓用户留存率和iOS用户转化率
 def get_user_device_count():
     total = Comment.select().where(Comment.user_device.is_null(False)).count()
     android = Comment.select().where(Comment.user_device == 'Android').count()
@@ -117,6 +118,7 @@ def get_mishop_total():
 
 # 计算各个型号的统计数据
 def get_model_count():
+    # 苏宁和京东数据合并
     all_color = ['国风雅灰', '钛银黑', '冰海蓝', '蜜桃金']
     all_ram = ['8GB', '12GB']
     all_rom = ['128GB', '256GB']
@@ -128,14 +130,193 @@ def get_model_count():
                     (ModelSummary.product_ram == ram) &
                     (ModelSummary.product_rom == rom)
                 )
+                # 没有12GB+128GB的奇葩内存组合
+                if len(all_ms) == 0:
+                    continue
+                mc = ModelCount.create(
+                    product_color=color,
+                    product_ram=ram,
+                    product_rom=rom
+                )
                 for ms in all_ms:
                     if ms.source == '京东':
-                        pass
+                        mc.total += ms.default_good + ms.star_one + ms.star_two + \
+                                    ms.star_three + ms.star_four + ms.star_five
+                        mc.cal_total += ms.star_one + ms.star_two + ms.star_three + ms.star_four + ms.star_five
+                        mc.save()
                     if ms.source == '苏宁':
-                        pass
+                        mc.total += ms.total
+                        mc.cal_total += ms.total
+                        mc.save()
+
+                    mc.default_good += ms.default_good
+                    mc.good_count += ms.star_four + ms.star_five
+                    mc.general_count += ms.star_two + ms.star_three
+                    mc.bad_count += ms.star_one
+                    mc.star_one += ms.star_one
+                    mc.star_two += ms.star_two
+                    mc.star_three += ms.star_three
+                    mc.star_four += ms.star_four
+                    mc.star_five += ms.star_five
+                    mc.save()
+
+    # 计算占比和好评率 (京东计算规则)
+    jd_total = Total.get(Total.source == '京东')
+    sn_total = Total.get(Total.source == '苏宁')
+    models_total = jd_total.all_models_total + sn_total.all_models_total
+    for mc in ModelCount.select():
+        mc.percentage = calculate_percentage(models_total, mc.total)
+        mc.good_rate = calculate_percentage(mc.cal_total, mc.good_count)
+        mc.save()
+
+
+# 计算不同颜色的统计数据
+def get_color_count():
+    jd_total = Total.get(Total.source == '京东')
+    sn_total = Total.get(Total.source == '苏宁')
+    models_total = jd_total.all_models_total + sn_total.all_models_total
+
+    all_color = ['国风雅灰', '钛银黑', '冰海蓝', '蜜桃金']
+    for color in all_color:
+        color_total = color_cal_total = color_default_good = 0
+        color_good_count = color_general_count = color_bad_count = 0
+        color_star_one = color_star_two = color_star_three = 0
+        color_star_four = color_star_five = 0
+
+        all_mc = ModelCount.select().where(ModelCount.product_color == color)
+        for mc in all_mc:
+            color_total += mc.total
+            color_cal_total += mc.cal_total
+            color_default_good += mc.default_good
+            color_good_count += mc.good_count
+            color_general_count += mc.general_count
+            color_bad_count += mc.bad_count
+            color_star_one += mc.star_one
+            color_star_two += mc.star_two
+            color_star_three += mc.star_three
+            color_star_four += mc.star_four
+            color_star_five += mc.star_five
+
+        ColorCount.create(
+            product_color=color,
+            total=color_total,
+            percentage=calculate_percentage(models_total, color_total),
+            cal_total=color_cal_total,
+            good_rate=calculate_percentage(color_cal_total, color_good_count),
+            default_good=color_default_good,
+            good_count=color_good_count,
+            general_count=color_general_count,
+            bad_count=color_bad_count,
+            star_one=color_star_one,
+            star_two=color_star_two,
+            star_three=color_star_three,
+            star_four=color_star_four,
+            star_five=color_star_five
+        )
+
+
+# 计算不同内存的统计数据
+def get_ram_count():
+    jd_total = Total.get(Total.source == '京东')
+    sn_total = Total.get(Total.source == '苏宁')
+    models_total = jd_total.all_models_total + sn_total.all_models_total
+
+    all_ram = ['8GB', '12GB']
+    for ram in all_ram:
+        ram_total = ram_cal_total = ram_default_good = 0
+        ram_good_count = ram_general_count = ram_bad_count = 0
+        ram_star_one = ram_star_two = ram_star_three = 0
+        ram_star_four = ram_star_five = 0
+
+        all_mc = ModelCount.select().where(ModelCount.product_ram == ram)
+        for mc in all_mc:
+            ram_total += mc.total
+            ram_cal_total += mc.cal_total
+            ram_default_good += mc.default_good
+            ram_good_count += mc.good_count
+            ram_general_count += mc.general_count
+            ram_bad_count += mc.bad_count
+            ram_star_one += mc.star_one
+            ram_star_two += mc.star_two
+            ram_star_three += mc.star_three
+            ram_star_four += mc.star_four
+            ram_star_five += mc.star_five
+
+        RamCount.create(
+            product_ram=ram,
+            total=ram_total,
+            percentage=calculate_percentage(models_total, ram_total),
+            cal_total=ram_cal_total,
+            good_rate=calculate_percentage(ram_cal_total, ram_good_count),
+            default_good=ram_default_good,
+            good_count=ram_good_count,
+            general_count=ram_general_count,
+            bad_count=ram_bad_count,
+            star_one=ram_star_one,
+            star_two=ram_star_two,
+            star_three=ram_star_three,
+            star_four=ram_star_four,
+            star_five=ram_star_five
+        )
+
+
+# 计算不同储存的统计数据
+def get_rom_count():
+    jd_total = Total.get(Total.source == '京东')
+    sn_total = Total.get(Total.source == '苏宁')
+    models_total = jd_total.all_models_total + sn_total.all_models_total
+
+    all_rom = ['128GB', '256GB']
+    for rom in all_rom:
+        rom_total = rom_cal_total = rom_default_good = 0
+        rom_good_count = rom_general_count = rom_bad_count = 0
+        rom_star_one = rom_star_two = rom_star_three = 0
+        rom_star_four = rom_star_five = 0
+
+        all_mc = ModelCount.select().where(ModelCount.product_rom == rom)
+        for mc in all_mc:
+            rom_total += mc.total
+            rom_cal_total += mc.cal_total
+            rom_default_good += mc.default_good
+            rom_good_count += mc.good_count
+            rom_general_count += mc.general_count
+            rom_bad_count += mc.bad_count
+            rom_star_one += mc.star_one
+            rom_star_two += mc.star_two
+            rom_star_three += mc.star_three
+            rom_star_four += mc.star_four
+            rom_star_five += mc.star_five
+
+        RomCount.create(
+            product_rom=rom,
+            total=rom_total,
+            percentage=calculate_percentage(models_total, rom_total),
+            cal_total=rom_cal_total,
+            good_rate=calculate_percentage(rom_cal_total, rom_good_count),
+            default_good=rom_default_good,
+            good_count=rom_good_count,
+            general_count=rom_general_count,
+            bad_count=rom_bad_count,
+            star_one=rom_star_one,
+            star_two=rom_star_two,
+            star_three=rom_star_three,
+            star_four=rom_star_four,
+            star_five=rom_star_five
+        )
+
+
+# 统计用户评论时间分布 (月度)
+def get_comment_date_count():
+    comment = Comment.get_by_id('SN810521815')
+    year_month = str(comment.create_time)[0:7]
+    print(year_month, type(year_month))
 
 
 if __name__ == '__main__':
     # get_user_device_count()
     # get_total()
-    get_model_count()
+    # get_model_count()
+    # get_color_count()
+    # get_ram_count()
+    # get_rom_count()
+    get_comment_date_count()
--- db/mi10_analyze_models.py
+++ db/mi10_analyze_models.py
@@ -46,23 +46,88 @@ class ModelCount(BaseModel):
                                  constraints=[pw.Check('product_color in ("国风雅灰", "钛银黑", "冰海蓝", "蜜桃金")')])  # 产品颜色版本
     product_ram = pw.CharField(max_length=4, constraints=[pw.Check('product_ram in ("8GB", "12GB")')])  # 内存大小
     product_rom = pw.CharField(max_length=5, constraints=[pw.Check('product_rom in ("128GB", "256GB")')])  # 储存大小
-    total = pw.IntegerField(null=True)
+    total = pw.IntegerField(default=0)
     percentage = pw.CharField(max_length=4, null=True)  # 该型号占总数的百分比
-    cal_total = pw.IntegerField(null=True)  # 用于计算好评率的总数 (京东计算规则)
+    cal_total = pw.IntegerField(default=0)  # 用于计算好评率的总数 (京东计算规则)
     good_rate = pw.CharField(max_length=4, null=True)  # 好评率
-    default_good = pw.IntegerField(null=True)  # 默认好评数
-    good_count = pw.IntegerField(null=True)  # 好评数
-    general_count = pw.IntegerField(null=True)  # 中评数
-    bad_count = pw.IntegerField(null=True)  # 差评数
-    star_one = pw.IntegerField(null=True)  # 一星数量
-    star_two = pw.IntegerField(null=True)  # 二星数量
-    star_three = pw.IntegerField(null=True)  # 三星数量
-    star_four = pw.IntegerField(null=True)  # 四星数量
-    star_five = pw.IntegerField(null=True)  # 五星数量
+    default_good = pw.IntegerField(default=0)  # 默认好评数
+    good_count = pw.IntegerField(default=0)  # 好评数
+    general_count = pw.IntegerField(default=0)  # 中评数
+    bad_count = pw.IntegerField(default=0)  # 差评数
+    star_one = pw.IntegerField(default=0)  # 一星数量
+    star_two = pw.IntegerField(default=0)  # 二星数量
+    star_three = pw.IntegerField(default=0)  # 三星数量
+    star_four = pw.IntegerField(default=0)  # 四星数量
+    star_five = pw.IntegerField(default=0)  # 五星数量
 
     class Meta:
         primary_key = pw.CompositeKey('product_color', 'product_ram', 'product_rom')  # 复合主键
 
 
+# 不同颜色的统计数据
+class ColorCount(BaseModel):
+    product_color = pw.CharField(max_length=4, primary_key=True,
+                                 constraints=[pw.Check('product_color in ("国风雅灰", "钛银黑", "冰海蓝", "蜜桃金")')])  # 产品颜色版本
+    total = pw.IntegerField()
+    percentage = pw.CharField(max_length=4)
+    cal_total = pw.IntegerField()
+    good_rate = pw.CharField(max_length=4)
+    default_good = pw.IntegerField()  # 默认好评数
+    good_count = pw.IntegerField()  # 好评数
+    general_count = pw.IntegerField()  # 中评数
+    bad_count = pw.IntegerField()  # 差评数
+    star_one = pw.IntegerField()  # 一星数量
+    star_two = pw.IntegerField()  # 二星数量
+    star_three = pw.IntegerField()  # 三星数量
+    star_four = pw.IntegerField()  # 四星数量
+    star_five = pw.IntegerField()  # 五星数量
+
+
+# 不同内存的统计数据
+class RamCount(BaseModel):
+    product_ram = pw.CharField(max_length=4, primary_key=True,
+                               constraints=[pw.Check('product_ram in ("8GB", "12GB")')])  # 内存大小
+    total = pw.IntegerField()
+    percentage = pw.CharField(max_length=4)
+    cal_total = pw.IntegerField()
+    good_rate = pw.CharField(max_length=4)
+    default_good = pw.IntegerField()  # 默认好评数
+    good_count = pw.IntegerField()  # 好评数
+    general_count = pw.IntegerField()  # 中评数
+    bad_count = pw.IntegerField()  # 差评数
+    star_one = pw.IntegerField()  # 一星数量
+    star_two = pw.IntegerField()  # 二星数量
+    star_three = pw.IntegerField()  # 三星数量
+    star_four = pw.IntegerField()  # 四星数量
+    star_five = pw.IntegerField()  # 五星数量
+
+
+# 不同储存的统计数据
+class RomCount(BaseModel):
+    product_rom = pw.CharField(max_length=5, primary_key=True,
+                               constraints=[pw.Check('product_rom in ("128GB", "256GB")')])  # 储存大小
+    total = pw.IntegerField()
+    percentage = pw.CharField(max_length=4)
+    cal_total = pw.IntegerField()
+    good_rate = pw.CharField(max_length=4)
+    default_good = pw.IntegerField()  # 默认好评数
+    good_count = pw.IntegerField()  # 好评数
+    general_count = pw.IntegerField()  # 中评数
+    bad_count = pw.IntegerField()  # 差评数
+    star_one = pw.IntegerField()  # 一星数量
+    star_two = pw.IntegerField()  # 二星数量
+    star_three = pw.IntegerField()  # 三星数量
+    star_four = pw.IntegerField()  # 四星数量
+    star_five = pw.IntegerField()  # 五星数量
+
+
+# 用户评论分布时间 (月度)
+class CommentDateCount(BaseModel):
+    year_month = pw.CharField(max_length=7, primary_key=True)
+    total = pw.IntegerField(default=0)
+    percentage = pw.CharField(max_length=4, null=True)
+
+
 if __name__ == '__main__':
-    mi10_analyze_db.create_tables([UserDeviceCount, Total, ModelCount])
+    # mi10_analyze_db.create_tables([UserDeviceCount, Total, ModelCount, ColorCount, RamCount, RomCount, CommentDateCount])
+    CommentDateCount.create_table()
