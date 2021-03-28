6fd441add9ce7bccef5ca2273513fee2d14e3ffd
Mark-ThinkPad <1372469698@qq.com>
2021-03-28 17:05:36 +0800

    2021-03-28 17:05:36

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
+
+
+@views.route('/mi10')
+def mi10():
+    return render_template('mi10.html')
